a34f9089403e1b70f6e4e6a9cb0feae4c0b7b692
é o kas <kastelo.jp@gmail.com>
2024-10-10 23:58:50 -0300

    Update dsf053.py

--- CURSO EM VÍDEO/MUNDO 2/AULA 13/dsf053.py
+++ CURSO EM VÍDEO/MUNDO 2/AULA 13/dsf053.py
@@ -9,3 +9,11 @@ a torre da derrota
 o lobo ama o bolo
 anotaram a data da maratona
 '''
+pal = input("Digite um palíndromo: ")
+trat = pal.lower().strip().replace(" ", "")
+inv = "".join(reversed(trat))
+
+if inv == trat:
+    print(f"A palavra \033[0;36m{pal}\033[m ao contrário fica \033[0;33m{"".join(reversed(pal))}\033[m\nESSA PALAVRA É UM \033[0;32mPALÍNDROMO!\033[m")
+else:
+    print(f"A palavra \033[0;36m{pal}\033[m ao contrário fica \033[0;33m{"".join(reversed(pal))}\033[m\nESSA PALAVRA \033[1mNÃO\033[m É UM \033[0;31mPALÍNDROMO\033[m")
