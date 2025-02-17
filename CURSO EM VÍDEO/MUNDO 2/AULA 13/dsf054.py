5c5365843191b26201d5a29e0cc55f2fcb7ace8b
é o kas <kastelo.jp@gmail.com>
2025-02-16 21:34:42 -0300

    att dsf55

--- CURSO EM VÍDEO/MUNDO 2/AULA 13/dsf054.py
+++ CURSO EM VÍDEO/MUNDO 2/AULA 13/dsf054.py
@@ -5,3 +5,22 @@ e quantas já são maiores
 
 *Considerando 21 anos como maioridade*
 '''
+
+from datetime import date
+
+atual = date.today().year
+
+maiores = 0
+menores = 0
+
+for i in range(1, 8):
+  nasc = int(input((f'Qual o ano do nascimento da {i}ª pessoa: ')))
+  idade = atual - nasc
+
+  if idade >= 21:
+    maiores += 1
+  else:
+    menores += 1
+
+print(f'No total, temos {maiores} pessoas maior de idade.')
+print(f'E também tivemos {menores} pessoas menores de idade.')
--- CURSO EM VÍDEO/MUNDO 2/AULA 13/dsf055.py
+++ CURSO EM VÍDEO/MUNDO 2/AULA 13/dsf055.py
@@ -2,3 +2,17 @@
 Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi
 o maior e o menor peso lidos.
 '''
+
+for i in range(1, 6):
+  peso = float(int(f'Digite o peso da {i}ª pessoa: '))
+
+  if i == 1:
+    maior = peso
+    menor = peso
+else:
+  if peso > maior:
+    maior = peso
+  if peso < menor:
+    menor = peso
+
+print(f'O maior peso lido foi{}')
