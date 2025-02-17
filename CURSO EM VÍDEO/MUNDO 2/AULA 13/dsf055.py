8beade6bfca95d799b399d8bffc339f1919f40d4
é o kas <kastelo.jp@gmail.com>
2025-02-17 00:58:59 -0300

    new challenges

--- CURSO EM VÍDEO/MUNDO 2/AULA 13/dsf055.py
+++ CURSO EM VÍDEO/MUNDO 2/AULA 13/dsf055.py
@@ -2,17 +2,19 @@
 Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi
 o maior e o menor peso lidos.
 '''
+maior = 0
+menor = 0
 
 for i in range(1, 6):
-  peso = float(int(f'Digite o peso da {i}ª pessoa: '))
-
+  peso = float(input(f'Digite o peso da {i}ª pessoa: '))
   if i == 1:
     maior = peso
     menor = peso
-else:
-  if peso > maior:
-    maior = peso
-  if peso < menor:
-    menor = peso
+  else:
+    if peso > maior:
+      maior = peso
+    if peso < menor:
+      menor = peso
 
-print(f'O maior peso lido foi{}')
+print(f'O maior peso lido foi {maior}Kg!')
+print(f'O menos peso lido foi {menor}Kg!')
--- CURSO EM VÍDEO/MUNDO 2/AULA 13/dsf056.py
+++ CURSO EM VÍDEO/MUNDO 2/AULA 13/dsf056.py
@@ -5,8 +5,29 @@ No final do programa, mostre:
 - Qual é o nome do homem mais velho.
 - Quantas mulheres tem menos de 20 anos.
 '''
+somaId = 0
+maiorId = 0
+nomVelho = ''
+mulher = 0
+
 for i in range(1, 5):
-    nome = input('Qual o seu nome? ')
-    idade = int(input('Qual a sua idade? '))
-    sexo = input('Feminino ou Masculino? ')
-med = 
+    print(f'----- {i} PESSOA -----')
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ').strip().lower())
+    
+    somaId += idade
+
+    if sexo == 'm' and idade > maiorId:
+        maiorId = idade
+        nomVelho = nome
+    if sexo == 'f' and idade < 20:
+        mulher += 1
+med = somaId / 4
+
+print(f'A média de idade do grupo é {med:.1f} anos.')
+if nomVelho:
+    print(f'O homem mais velho é {nomVelho} com {maiorId} anos!')
+else:
+    print('Não há homens no grupo!')
+print(f'Ao todo, há {mulher} mulheres com menos de 20 anos!')
