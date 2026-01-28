70e8057caafbcbb03c74af6fe1da43e2fb4381f9
é o kas <kastelo.jp@gmail.com>
2026-01-28 18:56:20 -0300

    more challenges

--- CURSO EM VÍDEO/MUNDO 2/AULA 15/dsf068.py
+++ CURSO EM VÍDEO/MUNDO 2/AULA 15/dsf068.py
@@ -1,3 +1,41 @@
 '''
-
+Faça um programa que jogue par ou impar com o computador.
+O jogo ó será interrompido quando o jogador PERDER, mostrando o total de vitórias
+consecutivas que ele conquistou no final do jogo.
 '''
+from random import randint
+
+n = c = s = v = 0
+p = ''
+print ('=-='*10)
+print('Vamos jogar Par ou Ímpar!')
+print ('=-='*10)
+
+while True: 
+    p = input('Escolha Par ou Ímpar? [P/I]: ').upper().strip()
+    n = int(input('Escolha um número: '))
+    c = randint(0, 10)
+    s = c + n
+    print('---'*10)
+    if s % 2 == 0:
+        print(f'Você jogou {n} e o computador {c}. Total de {s}, DEU PAR')
+    else:
+        print(f'Você jogou {n} e o computador {c}. Total de {s}, DEU ÍMPAR')
+    print('---'*10)
+    if p == 'P':
+        if s % 2 == 0:
+            print ('Você VENCEU!')
+        else:
+            print('Você PERDEU!')
+            break
+    elif p == 'I':
+        if s % 2 == 0:
+            print('Você PERDEU!')
+            break
+        else: print('Você VENCEU!')
+    v += 1
+    print('Vamo jogar novamente...')
+    print('=-='*10)
+print(f'Você perdeu com {v} vitórias consecutivas!')
+
+    
--- CURSO EM VÍDEO/MUNDO 2/AULA 15/dsf069.py
+++ CURSO EM VÍDEO/MUNDO 2/AULA 15/dsf069.py
@@ -1,3 +1,43 @@
 '''
+Crie um programa que leia a idade e o 
+sexo de várias pessoas. A cada pessoa cadastrada, o programa
+deverá peruntar se o usuário quer ou não continuar.
+No final, mostre:
 
+A) quantas pessoas tem mais de 18 anos
+B) Quantos homens foram cadastrados
+C) quantas mulheres tem menos de 20 anos.
 '''
+
+h = m = i = 0
+
+p = ""
+cont = ''
+print('-'*30)
+print('   CADASTRE UMA PESSOA   ')
+print('-'*30)
+
+Id = 0
+
+while True: 
+    while True:
+        Id = int(input('Idade: '))
+        if Id >= 18:
+            i =+1
+        break
+    while True:
+        p = str(input('Sexo: [M/F]')).strip().upper()
+        if p in ("M", "F"):
+            if p == "M":
+                h+=1
+            elif p == "F":
+                if Id >=20:
+                    m+=1  
+            break
+    cont = str(input('Quer continuar? [S/N]')).strip().upper()
+    if cont in ('S', 'N'):
+        if cont == 'N':
+            break
+
+print(f'===== FIM DO PROGRAMA =====')
+print(f'Total de pessoas com mais de 18 anos: {i}\nTotal de homens registrados: {h}\nTotal de mulheres acima de 20 anos: {m}')
