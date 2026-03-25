1c225560057d4cbb6786733429dcb37fbfacc18c
é o kas <kastelo.jp@gmail.com>
2026-03-24 23:38:41 -0300

    dsf 72 and 73 solved

--- CURSO EM VÍDEO/MUNDO 3/AULA 16/dsf72.py
+++ CURSO EM VÍDEO/MUNDO 3/AULA 16/dsf72.py
@@ -6,3 +6,13 @@ até vinte.
 Seu programa deverá ler um número pelo teclado
 (entre 0 e 20) e mostrá-lo por extenso.
 '''
+
+cont = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
+
+per = int(input('Digite um número entre  0 a 20: '))
+
+while per < 0 or per > 20:
+    print("Tente novamente.", end=' ')
+    per = int(input("Digite um número entre 0 e 20: "))
+
+print(f"O número que você digitou foi {cont[per]}.")
--- CURSO EM VÍDEO/MUNDO 3/AULA 16/dsf73.py
+++ CURSO EM VÍDEO/MUNDO 3/AULA 16/dsf73.py
@@ -8,3 +8,15 @@ b) os últimos 4 colocados da tabela
 c) Uma lista com os times em ordem alfabética.
 d) Em que posição na tabela esta o time da chapecoense.
 '''
+
+times = "Palmeiras", "São Paulo", 'Fluminense', 'Flamengo', 'Bahia', 'Athletico-PR', 'Coritiba', 'Grêmio', 'Vasco da Gama', 'EC Vitória', 'Corinthians', 'Internacional', 'Atlético-MG', 'Bragantino', 'Chapecoense', 'Santos', 'Botafogo', 'Mirassol', 'Remo', 'Cruzeiro'
+print("-=-"*10)
+print(f"Lista dos times do Brasileirão: {times}")
+print("-=-"*10)
+print(f'Os 5 primeiros colocados do Brasileirão são: {times[0:4]}')
+print("-=-"*10)
+print(f"Os últimos 4 colocados da lista são: {times[-4:]}")
+print("-=-"*10)
+print(f"Em ordem alfabética: {sorted(times)}")
+print("-=-"*10)
+print(f"A Chapecoense está na {times.index("Chapecoense")}ª posição")
